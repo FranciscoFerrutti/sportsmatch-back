d770c7f3220327281bd0b159fe88a3b2526e4795
sol <skonfederak@itba.edu.ar>
2024-10-07 20:20:02 -0300

    comment cors

--- src/routes/events.routes.ts
+++ src/routes/events.routes.ts
@@ -15,7 +15,7 @@ export default class EventsRoutes {
 
     public init(): void {
         this.router.use(urlencoded({ extended: true }));
-        this.router.use(cors());
+        // this.router.use(cors());
 
         this.router.get('/', this.controller.getEvents);
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -15,7 +15,7 @@ export default class UsersRoutes {
 
     public init(): void {
         this.router.use(urlencoded({ extended: true }));
-        this.router.use(cors());
+        // this.router.use(cors());
 
         this.router.get('/', this.controller.getUsers);
         this.router.get('/:userId', this.controller.getUser);
